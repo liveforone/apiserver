ca742e09538ca4a9d62c45c48a10b7fc6df2bc6d
liveforone <yc4852@gmail.com>
2022-05-25 16:26:41 +0900

    Good Commit

---
+++ src/test/java/apiserver/apiserver/controller/member/MemberControllerIntegrationTest.java
@@ -0,0 +1,140 @@
+package apiserver.apiserver.controller.member;
+
+import apiserver.apiserver.dto.sign.SignInRequest;
+import apiserver.apiserver.dto.sign.SignInResponse;
+import apiserver.apiserver.entity.member.Member;
+import apiserver.apiserver.exception.MemberNotFoundException;
+import apiserver.apiserver.init.TestInitDB;
+import apiserver.apiserver.repository.member.MemberRepository;
+import apiserver.apiserver.service.sign.SignService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest  //Spring Security에 관한 테스트는 @SpringBootTest를 선언하여 통합 테스트로 진행하도록 하겠습니다.
+@AutoConfigureMockMvc
+@ActiveProfiles(value = "test") //기존 initDB와의 충돌(걔는 local로 설정되잇음)때문에 따로 선언
+@Transactional
+public class MemberControllerIntegrationTest {
+
+    @Autowired
+    WebApplicationContext context;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    TestInitDB initDB;
+
+    @Autowired
+    SignService signService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+        initDB.initDB();
+    }
+
+    @Test
+    void readTest() throws Exception {
+        // given
+        Member member = memberRepository.findByEmail(initDB.getMember1Email()).orElseThrow(MemberNotFoundException::new);
+
+        // when, then
+        mockMvc.perform(
+                        get("/api/members/{id}", member.getId()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteTest() throws Exception {
+        // given
+        Member member = memberRepository.findByEmail(initDB.getMember1Email()).orElseThrow(MemberNotFoundException::new);
+        SignInResponse signInRes = signService.signIn(new SignInRequest(initDB.getMember1Email(), initDB.getPassword()));
+
+        // when, then
+        mockMvc.perform(
+                        delete("/api/members/{id}", member.getId()).header("Authorization", signInRes.getAccessToken()))
+                .andExpect(status().isOk());
+    }
+
+    /*
+    관리자에 의한 삭제 요청입니다.
+     */
+    @Test
+    void deleteByAdminTest() throws Exception {
+        // given
+        Member member = memberRepository.findByEmail(initDB.getMember1Email()).orElseThrow(MemberNotFoundException::new);
+        SignInResponse adminSignInRes = signService.signIn(new SignInRequest(initDB.getAdminEmail(), initDB.getPassword()));
+
+        // when, then
+        mockMvc.perform(
+                        delete("/api/members/{id}", member.getId()).header("Authorization", adminSignInRes.getAccessToken()))
+                .andExpect(status().isOk());
+    }
+
+    /*
+    인증되지 않은 사용자가 요청을 전송하였습니다.
+     */
+    @Test
+    void deleteUnauthorizedByNoneTokenTest() throws Exception {
+        // given
+        Member member = memberRepository.findByEmail(initDB.getMember1Email()).orElseThrow(MemberNotFoundException::new);
+
+        // when, then
+        mockMvc.perform(
+                        delete("/api/members/{id}", member.getId()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/exception/entry-point"));
+    }
+
+    /*
+    인증된 사용자가 자신의 자원이 아닌, 남의 자원에 접근하는 것을 요청하였습니다
+     */
+    @Test
+    void deleteAccessDeniedByNotResourceOwnerTest() throws Exception {
+        // given
+        Member member = memberRepository.findByEmail(initDB.getMember1Email()).orElseThrow(MemberNotFoundException::new);
+        SignInResponse attackerSignInRes = signService.signIn(new SignInRequest(initDB.getMember2Email(), initDB.getPassword()));
+
+        // when, then
+        mockMvc.perform(
+                        delete("/api/members/{id}", member.getId()).header("Authorization", attackerSignInRes.getAccessToken()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/exception/access-denied"));
+    }
+
+    /*
+    정상적인 사용자이지만 리프레시 토큰으로 요청하였을 경우, 요청을 제한하며
+    CustomAccessDeniedHandler가 작동하며,
+    3xx 상태 코드를 응답받아서 /exception/access-denied로 리다이렉트됩니다.
+     */
+    @Test
+    void deleteAccessDeniedByRefreshTokenTest() throws Exception {
+        // given
+        Member member = memberRepository.findByEmail(initDB.getMember1Email()).orElseThrow(MemberNotFoundException::new);
+        SignInResponse signInRes = signService.signIn(new SignInRequest(initDB.getMember1Email(), initDB.getPassword()));
+
+        // when, then
+        mockMvc.perform(
+                        delete("/api/members/{id}", member.getId()).header("Authorization", signInRes.getRefreshToken()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/exception/access-denied"));
+    }
+}
---
+++ src/test/java/apiserver/apiserver/init/TestInitDB.java
@@ -0,0 +1,80 @@
+package apiserver.apiserver.init;
+
+import apiserver.apiserver.entity.member.Member;
+import apiserver.apiserver.entity.member.Role;
+import apiserver.apiserver.entity.member.RoleType;
+import apiserver.apiserver.exception.RoleNotFoundException;
+import apiserver.apiserver.repository.member.MemberRepository;
+import apiserver.apiserver.repository.role.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class TestInitDB {
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    private String adminEmail = "admin@admin.com";
+    private String member1Email = "member1@member.com";
+    private String member2Email = "member2@member.com";
+    private String password = "123456a!";
+
+    private void initRole() {
+        roleRepository.saveAll(
+                List.of(RoleType.values()).stream().map(roleType -> new Role(roleType)).collect(Collectors.toList())
+        );
+    }
+
+    private void initTestAdmin() {
+        memberRepository.save(
+                new Member(adminEmail, passwordEncoder.encode(password), "admin", "admin",
+                        List.of(roleRepository.findByRoleType(RoleType.ROLE_NORMAL).orElseThrow(RoleNotFoundException::new),
+                                roleRepository.findByRoleType(RoleType.ROLE_ADMIN).orElseThrow(RoleNotFoundException::new)))
+        );
+    }
+
+    private void initTestMember() {
+        memberRepository.saveAll(
+                List.of(
+                        new Member(member1Email, passwordEncoder.encode(password), "member1", "member1",
+                                List.of(roleRepository.findByRoleType(RoleType.ROLE_NORMAL).orElseThrow(RoleNotFoundException::new))),
+                        new Member(member2Email, passwordEncoder.encode(password), "member2", "member2",
+                                List.of(roleRepository.findByRoleType(RoleType.ROLE_NORMAL).orElseThrow(RoleNotFoundException::new))))
+        );
+    }
+
+    @Transactional
+    public void initDB() {
+        initRole();
+        initTestAdmin();
+        initTestMember();
+    }
+
+    public String getAdminEmail() {
+        return adminEmail;
+    }
+
+    public String getMember1Email() {
+        return member1Email;
+    }
+
+    public String getMember2Email() {
+        return member2Email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
